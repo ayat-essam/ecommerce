bc593b3fedce27f115803e41131eb1f73e84dcfb
Ayat Esaam <eayatessam1@gmail.com>
2024-10-17 02:06:16 +0300

    Implement Ui

---
+++ lib/feature/auth/data/data_sources/local/auth_shared_pref_local_data_source.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerce/core/constants.dart';
+import 'package:ecommerce/core/error/exceptions.dart';
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'auth_local_data_source.dart';
+
+class AuthSharedPrefLocalDataSource extends AuthLocalDataSource {
+  @Singleton(as: AuthLocalDataSource)
+final SharedPreferences sharedpref;
+AuthSharedPrefLocalDataSource(this.sharedpref);
+
+  @override
+  Future<void> saveToken(String token) async {
+    try {
+      await sharedpref.setString(CacheConstants.tokenKey, token);
+    }catch(exception){
+      throw const LocalException('failed to save token');
+    }
+  }
+
+  @override
+  Future<String> getToken() async {
+    try{
+      return sharedpref.getString(CacheConstants.tokenKey)!;
+    }catch(exception){
+      throw const LocalException('failed to get token');
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/app_bloc_observer.dart';
+import 'core/di/service_locator.dart';
+import 'core/routes/route_generator.dart';
+import 'core/routes/routes.dart';
+import 'feature/auth/presentation/cubit/auth_cubit.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
+  await configureDependencies();
+  runApp(const ECommerceApp());
+}
+
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => serviceLocator.get<AuthCubit>(),
+      child: ScreenUtilInit(
+        designSize: const Size(430, 932),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) => const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.getRoute,
+          initialRoute: Routes.register,
+        ),
+      ),
+    );
+  }
+}
